46fe0ce67dfffd485b07bca2c170f5ae7edb820c
Kallarackal, Jim <jkallarackal@deloitte.de>
2023-01-09 09:46:14 +0100

    initial draft

---
+++ src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+<v-container class="fill-height">
+  <v-responsive class="d-flex align-center text-center fill-height">
+    <v-img
+      contain
+      height="300"
+      src="@/assets/logo.svg"
+    />
+
+    <div class="text-body-2 font-weight-light mb-n1">Login to </div>
+
+    <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+
+    <div class="py-14" />
+
+    <v-row class="d-flex align-center justify-center">
+      <v-col cols="auto">
+        <v-btn
+          min-width="164"
+          rel="noopener noreferrer"
+          target="_blank"
+          variant="text"
+        >
+          <v-icon
+            icon="mdi-view-dashboard"
+            size="large"
+            start
+          />
+
+          Login
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-responsive>
+</v-container>
+</template>
+
+<script setup>
+
+</script>

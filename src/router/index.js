215d60b6b743ee2ba83e0959fd3feca4182e6997
Jim Kallarackal <jim.kallarackal@googlemail.com>
2023-01-09 14:06:36 +0000

    Update index.js and Login.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,28 @@
 // Composables
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
+    path: "/",
+    component: () => import("@/layouts/default/Default.vue"),
     children: [
       {
-        path: '',
-        name: 'Home',
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
+        path: "/",
+        name: "Login",
+        component: () => import("@/views/Login.vue"),
       },
       {
-        path: '/login',
-        name: 'Login',
-        component: () => import('@/views/Login.vue'),
+        path: "/home",
+        name: "Home",
+        component: () => import("@/views/Home.vue"),
       },
     ],
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
-})
+});
 
-export default router
+export default router;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,40 +1,65 @@
 <template>
-<v-container class="fill-height">
-  <v-responsive class="d-flex align-center text-center fill-height">
-    <v-img
-      contain
-      height="300"
-      src="@/assets/logo.svg"
-    />
-
-    <div class="text-body-2 font-weight-light mb-n1">Login to </div>
-
-    <h1 class="text-h2 font-weight-bold">Vuetify</h1>
-
-    <div class="py-14" />
-
+  <v-container>
     <v-row class="d-flex align-center justify-center">
       <v-col cols="auto">
-        <v-btn
-          min-width="164"
-          rel="noopener noreferrer"
-          target="_blank"
-          variant="text"
-        >
-          <v-icon
-            icon="mdi-view-dashboard"
-            size="large"
-            start
-          />
+        <v-card class="mx-auto" max-width="344" outlined color="indigo">
+          <v-list-item three-line>
+            <v-list-item-content class="py-3">
+              <div class="text-overline mb-4">OVERLINE</div>
+              <v-list-item-title class="text-h5 mb-1">
+                Login
+              </v-list-item-title>
+              <v-list-item-subtitle
+                >Greyhound divisely hello coldly
+                fonwderfully</v-list-item-subtitle
+              >
+            </v-list-item-content>
+            <v-form ref="form" v-model="valid" class="py-3">
+              <v-text-field :counter="10" label="Name" required></v-text-field>
 
-          Login
-        </v-btn>
+              <v-text-field
+                v-model="email"
+                label="E-mail"
+                required
+              ></v-text-field>
+
+              <v-select
+                v-model="select"
+                :items="items"
+                :rules="[(v) => !!v || 'Item is required']"
+                label="Item"
+                required
+              ></v-select>
+
+              <v-checkbox
+                v-model="checkbox"
+                :rules="[(v) => !!v || 'You must agree to continue!']"
+                label="Do you agree?"
+                required
+              ></v-checkbox>
+
+              <v-btn
+                :disabled="!valid"
+                color="success"
+                class="mr-4"
+                @click="validate"
+              >
+                Validate
+              </v-btn>
+
+              <v-btn color="warning" @click="resetValidation">
+                Reset Validation
+              </v-btn>
+            </v-form>
+          </v-list-item>
+
+          <v-card-actions>
+            <v-btn outlined rounded text> Button </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
-  </v-responsive>
-</v-container>
+  </v-container>
 </template>
 
-<script setup>
-
-</script>
+<script setup></script>
